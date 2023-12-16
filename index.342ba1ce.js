597f0e60323b7bbcfa80f333c859ad3320595b61
Marina Rohozhynska <rogozhinskaya.marina.55@gmail.com>
2023-12-17 01:58:46 +0200

    make build

---
+++ index.342ba1ce.js
@@ -0,0 +1,3 @@
+window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?document.getElementById("myBtn").style.display="block":document.getElementById("myBtn").style.display="none"},document.getElementById("myBtn").addEventListener("click",function(){document.documentElement.scrollTop=0});//# sourceMappingURL=index.342ba1ce.js.map
+
+//# sourceMappingURL=index.342ba1ce.js.map
